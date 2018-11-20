e94da163a5e4ee220aed7a49bb01d0502483be30
ferdiego426 <42880955+ferdiego426@users.noreply.github.com>
2018-11-20 11:22:03 -0600

    Add files via upload

---
+++ proyectofinal/consecuencias.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <title>Rio Sabinal</title>
+    <link rel="stylesheet" href="css/estilos.css">
+  </head>
+  <body>
+    <br>
+    <p></p>
+    <p> </p>
+    <center><img src="imagenes/unachfca.jpg" alt=""></center>
+    <header style="height: 57.5373px; margin-top: 36.841px; width: 952.328px;">
+      <nav class="menu">
+        <ul>
+          <li style="margin-top: -4.48855px;"> <a href="index.html"> <span style="color: #333333;"><b
+
+                  style="font-family: Times New Roman;">Inicio </b></span></a></li>
+          <span style="color: #333333;"> <span style="font-family: Times New Roman;">
+            </span> </span>
+        </ul>
+        <span style="color: #333333;"> <span style="font-family: Times New Roman;">
+          </span> </span>
+        <ul>
+          <li><span style="color: #333333;"><b style="font-family: Times New Roman;">Historia
+                </b></span> <ul>
+              <li> <span style="color: #333333;"><a href="historia.php" style="font-family: Times New Roman;">Historia
+                    del rio</a></span></li>
+            </ul>
+          </li>
+        </ul>
+        <span style="color: #333333;"> <span style="font-family: Times New Roman;">
+          </span> </span>
+        <ul>
+          <li><span style="color: #333333;"><b style="font-family: Times New Roman;">Causas
+                </b></span> <ul>
+              <li><span style="color: #333333;"><span style="font-family: Times New Roman;"><a
+
+                      href="causas.php"> <span style="font-family: Times New Roman;">Causas
+                        de la contaminación</span></a></span></span></li>
+            </ul>
+          </li>
+        </ul>
+        <span style="color: #333333;"> <span style="font-family: Times New Roman;">
+          </span> </span>
+        <ul>
+          <li><span style="color: #333333;"><b style="font-family: Times New Roman;">Consecuencias</b></span>
+            <ul>
+              <li><span style="color: #333333;"><span style="font-family: Times New Roman;"><a
+
+                      href="consecuencias.php"> <span style="font-family: Times New Roman;">Consecuencias
+                        de la contaminación</span></a></span></span></li>
+              <span style="color: #333333;"><a href="consecuencias.php"> </a></span>
+            </ul>
+            <span style="color: #333333;"><a href="consecuencias.php"> </a></span></li>
+          <span style="color: #333333;"><a href="consecuencias.php"> </a></span>
+        </ul>
+        <span style="color: #333333;"> <a href="consecuencias.php"> <span style="font-family: Times New Roman;">
+            </span> </a> </span>
+        <ul>
+          <span style="color: #333333;"><a href="consecuencias.php"> </a></span>
+          <li><span style="color: #333333;"><b style="font-family: Times New Roman;">Situación</b></span>
+            <ul>
+              <li><span style="color: #333333;"><span style="font-family: Times New Roman;"><a
+
+                      href="situacion.php"> <span style="font-family: Times New Roman;">Situación
+                        real </span></a></span></span></li>
+            </ul>
+          </li>
+        </ul>
+        <span style="color: #333333;"> <span style="font-family: Times New Roman;">
+          </span> </span>
+        <ul>
+          <span style="color: #333333;"> <span style="font-family: Times New Roman;">
+            </span> </span>
+          <li><span style="color: #333333;"><b style="font-family: Times New Roman;">¿Qué
+                se hizo?</b></span>
+            <ul>
+              <li><span style="font-family: Times New Roman; color: white;"><a href="que.php">
+                    <span style="font-family: Times New Roman; color: white;">Evidencia
+                      </span></a></span></li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+      <h1> </h1>
+      <br>
+      <!--<center><img src="imagenes/planetcar.jpg" WIDTH=350 HEIGHT=250  VSPACE=3 HSPACE=20></center>-->
+    </header>
+    <center>
+      <h4>
+        <br>
+        <br>
+        <br>
+        <span style="font-family: Lucida Bright; color: #a3a3a3;">En cuanto al
+          mal olor que tiene el río Sabinal en su periferia, se debe al bajo
+          registro de niveles de oxígeno disuelto, lo que detiene <br>
+          el proceso aeróbico del agua para dar paso al proceso anaeróbico, lo
+          que hace que el agua respire de manera lenta y mal oliente.
+          <br>
+          <br>
+          Por otro lado el agua contaminada transporta un número incalculable de
+          bacterias, además de gusanos, lombrices y virus.
+          <br>
+          De estar en contacto y/o ser consumida por el ser humano, puede
+          desencadenar enfermedades como fiebre tifoidea, disentería, cólera,
+          entre otros.
+          <br>
+          <br>
+          Ante el incremento de enfermedades relacionadas con la calidad del
+          agua, darle la importancia requerida al tema de la contaminación del
+          <br>
+          río Sabinal, pues más allá de ofrecer un mal aspecto de la ciudad,
+          forma es un peligro latente para la salud y el ambiente.
+        </span></h4>
+      <p><br>
+      </p>
+      <img src="https://www.informador.mx/__export/1506043752678/sites/elinformador/img/historico/0/809569.jpg_1970638775.jpg"
+        alt="" style="width: 425px; height: 269px;">
+      <center>
+      </center>
+    </center>
+  </body>
+</html>
---
+++ proyectofinal/situacion.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <title>Rio Sabinal</title>
+    <link rel="stylesheet" href="css/estilos.css">
+  </head>
+  <body>
+    <br>
+    <p></p>
+    <p> </p>
+    <center><img src="imagenes/unachfca.jpg" alt=""></center>
+    <header style="height: 57.5373px; margin-top: 36.841px; width: 952.328px;">
+      <nav class="menu">
+        <ul>
+          <li style="margin-top: -4.48855px;"> <a href="index.html"> <span style="color: #333333;"><b
+
+                  style="font-family: Times New Roman;">Inicio </b></span></a></li>
+          <span style="color: #333333;"> <span style="font-family: Times New Roman;">
+            </span> </span>
+        </ul>
+        <span style="color: #333333;"> <span style="font-family: Times New Roman;">
+          </span> </span>
+        <ul>
+          <li><span style="color: #333333;"><b style="font-family: Times New Roman;">Historia
+                </b></span> <ul>
+              <li> <span style="color: #333333;"><a href="historia.php" style="font-family: Times New Roman;">Historia
+                    del rio</a></span></li>
+            </ul>
+          </li>
+        </ul>
+        <span style="color: #333333;"> <span style="font-family: Times New Roman;">
+          </span> </span>
+        <ul>
+          <li><span style="color: #333333;"><b style="font-family: Times New Roman;">Causas
+                </b></span> <ul>
+              <li><span style="color: #333333;"><span style="font-family: Times New Roman;"><a
+
+                      href="causas.php"> <span style="font-family: Times New Roman;">Causas
+                        de la contaminación</span></a></span></span></li>
+            </ul>
+          </li>
+        </ul>
+        <span style="color: #333333;"> <span style="font-family: Times New Roman;">
+          </span> </span>
+        <ul>
+          <li><span style="color: #333333;"><b style="font-family: Times New Roman;">Consecuencias</b></span>
+            <ul>
+              <li><span style="color: #333333;"><span style="font-family: Times New Roman;"><a
+
+                      href="consecuencias.php"> <span style="font-family: Times New Roman;">Consecuencias
+                        de la contaminación</span></a></span></span></li>
+              <span style="color: #333333;"><a href="consecuencias.php"> </a></span>
+            </ul>
+            <span style="color: #333333;"><a href="consecuencias.php"> </a></span></li>
+          <span style="color: #333333;"><a href="consecuencias.php"> </a></span>
+        </ul>
+        <span style="color: #333333;"> <a href="consecuencias.php"> <span style="font-family: Times New Roman;">
+            </span> </a> </span>
+        <ul>
+          <span style="color: #333333;"><a href="consecuencias.php"> </a></span>
+          <li><span style="color: #333333;"><b style="font-family: Times New Roman;">Situación</b></span>
+            <ul>
+              <li><span style="color: #333333;"><span style="font-family: Times New Roman;"><a
+
+                      href="situacion.php"> <span style="font-family: Times New Roman;">Situación
+                        real </span></a></span></span></li>
+            </ul>
+          </li>
+        </ul>
+        <span style="color: #333333;"> <span style="font-family: Times New Roman;">
+          </span> </span>
+        <ul>
+          <span style="color: #333333;"> <span style="font-family: Times New Roman;">
+            </span> </span>
+          <li><span style="color: #333333;"><b style="font-family: Times New Roman;">¿Qué
+                se hizo?</b></span>
+            <ul>
+              <li><span style="font-family: Times New Roman; color: white;"><a href="que.php">
+                    <span style="font-family: Times New Roman; color: white;">Evidencia
+                      </span></a></span></li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+      <h1> </h1>
+      <br>
+      <!--<center><img src="imagenes/planetcar.jpg" WIDTH=350 HEIGHT=250  VSPACE=3 HSPACE=20></center>-->
+    </header>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <center>
+      <h3>
+        <span style="font-family: Lucida Bright; color: #a3a3a3;">El rio sabinal
+          nace en un ojo de agua en el municipio de Berriozábal, transita por
+          toda la ciudad de Tuxtla Gutiérrez
+          <br>
+          y desemboca en el rio Grijalva pasando por el cañón del sumidero. <br>
+          Las autoridades de Protección Civil prevén que el recolector flotante
+          será más usado cuando termine el estiaje <br>
+          y comiencen las lluvias: "El problema de contaminación crecerá, porque
+          los torrentes pluviales arrastran la basura que hay en las calles de <br>
+          ciudades que colindan con el río, como Tuxtla y Chiapa de Corzo y
+          otras de las regiones Centro y Frailesca... <br>
+          vienen principalmente troncos, papel, cartón, bolsas y botellas de
+          plástico e incluso llantas, pilas, baterías <br>
+          y envases de aerosoles entre otros tantos objetos de uso personal”.
+        </span>
+      </h3>
+      <h3>
+        <br>
+        <br>
+        <center><img src="imagenes/riosabinal8.jpg" style="width: 434px; height: 250px;"
+            hspace="20" vspace="3"><br>
+          <h6><span style="color: white;"><u>Foto tomada por Gloria Fernada
+                Diego Hernández</u></span></h6>
+        </center>
+        <center>
+        </center>
+      </h3>
+    </center>
+  </body>
+</html>
